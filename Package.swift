ef56b9279a9a14a6a05175ff879db12845cc0fa2
Роман <>
2020-04-26 12:49:25 +1000

    .

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MyLibraryUI",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -24,5 +25,6 @@ let package = Package(
         .testTarget(
             name: "MyLibraryUITests",
             dependencies: ["MyLibraryUI"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
